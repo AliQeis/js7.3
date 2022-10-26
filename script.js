42f69278cfcb64c346892f22999183657556fed1
jschaik <9020512@student.zadkine.nl>
2022-10-26 14:12:30 +0200

    7.3 js done

---
+++ script.js
@@ -0,0 +1,10 @@
+ let getal=0;
+ function myfuction(){
+    let button=document.getElementById("aaa");
+let p=document.getElementById("bbb");
+p.innerHTML=getal++;    
+if(getal>10)
+{
+    getal=0;
+}
+ }
